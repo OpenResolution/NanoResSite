c9c924feb39698a9f0e781c08dd0ba6d81eaaf2b
Rongkang Xiong <earth@mail.ustc.edu.cn>
2023-09-24 14:04:39 +0800

    feature: add baidu-analytics

---
+++ source/js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0d2932ca2535e512691103abd7891c43";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
